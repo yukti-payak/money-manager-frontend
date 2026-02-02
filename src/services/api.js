8f4d29267749fe728d02e993210688cc621db0a6
Yukti Payak <21301064yukti@viva-technology.org>
2026-02-02 14:46:16 +0530

    fixing api.js code

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,26 @@
+// import axios from 'axios';
+
+
+// const API = axios.create({ baseURL: 'https://money-manager-backend-cr19.onrender.com/api/transactions' });
+
+// export const fetchTransactions = (params) => API.get('/transactions', { params });
+// export const addTransaction = (data) => API.post('/transactions', data);
+
+// export const updateTransaction = (id, data) => API.put(`/transactions/${id}`, data);
+// export const deleteTransaction = (id) => API.delete(`/transactions/${id}`);
+
+
+
 import axios from 'axios';
 
+const API = axios.create({ 
+  baseURL: 'https://money-manager-backend-cr19.onrender.com/api/transactions' 
+});
 
-const API = axios.create({ baseURL: 'https://money-manager-backend-cr19.onrender.com' });
 
-export const fetchTransactions = (params) => API.get('/transactions', { params });
-export const addTransaction = (data) => API.post('/transactions', data);
+export const fetchTransactions = (params) => API.get('/', { params });
+export const addTransaction = (data) => API.post('/', data);
 
-export const updateTransaction = (id, data) => API.put(`/transactions/${id}`, data);
-export const deleteTransaction = (id) => API.delete(`/transactions/${id}`);
+
+export const updateTransaction = (id, data) => API.put(`/${id}`, data);
+export const deleteTransaction = (id) => API.delete(`/${id}`);
